c4c3a592ad920a7a52ed85365527190adfa6a941
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-01-31 14:05:45 -0500

    fullscreen fix?

--- script.js
+++ script.js
@@ -1,6 +1,27 @@
-function openPage(url) {
-    const iframeContainer = document.getElementById('iframe-container');
-    const iframe = document.getElementById('iframe');
-    iframe.src = url;
-    iframeContainer.style.display = 'block';
-  }
+const iframe = document.getElementById('iframe');
+iframe.src = url;
+iframeContainer.style.display = 'block';
+
+if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.webkitRequestFullscreen) { 
+    iframe.webkitRequestFullscreen(); // Safari
+  } else if (iframe.msRequestFullscreen) { 
+    iframe.msRequestFullscreen(); // IE11
+}
+document.addEventListener('keydown', (event) => {
+  if (event.key === 'Escape') {
+    closeFullscreen();
+}
+});
+function closeFullscreen() {
+  if (document.exitFullscreen) {
+    document.exitFullscreen();
+  } else if (document.webkitExitFullscreen) {
+    document.webkitExitFullscreen(); // Safari
+  } else if (document.msExitFullscreen) {
+    document.msExitFullscreen(); // IE11
+}
+const iframeContainer = document.getElementById('iframe-container');
+  iframeContainer.style.display = 'none';
+}
