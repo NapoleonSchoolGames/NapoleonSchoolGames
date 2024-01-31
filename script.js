5978061802dfa7cd75b429f1d2cd9ca2026ed37f
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-01-31 14:30:25 -0500

    ok its back how it was please just wok

--- script.js
+++ script.js
@@ -1,6 +1,6 @@
-const iframeContainer = document.getElementById('iframe-container');
-const iframe = document.getElementById('iframe');
 function openPage(url) {
+  const iframeContainer = document.getElementById('iframe-container');
+  const iframe = document.getElementById('iframe');
   iframe.src = url;
   iframeContainer.style.display = 'block';
   
