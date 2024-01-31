7bf820c1b0979487c1d5b76f87fdd9cd59c1c9a6
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-01-31 13:45:09 -0500

    disable fullscreen to fix games, disable dark mode fix for ADR

--- games/adarkroom/script/engine.js
+++ games/adarkroom/script/engine.js
@@ -538,11 +538,11 @@
       var darkCss = Engine.findStylesheet('darkenLights');
       if (darkCss == null) {
         $('head').append('<link rel="stylesheet" href="css/dark.css" type="text/css" title="darkenLights" />');
-        $('.lightsOff').text(_('lights on. (broken)'));
+        $('.lightsOff').text(_('lights on.'));
         $SM.set('config.lightsOff', true, true);
       } else if (darkCss.disabled) {
         darkCss.disabled = false;
-        $('.lightsOff').text(_('lights on. (broken)'));
+        $('.lightsOff').text(_('lights on.'));
         $SM.set('config.lightsOff', true,true);
       } else {
         $("#darkenLights").attr("disabled", "disabled");
@@ -939,5 +939,4 @@ $.Dispatch = function( id ) {
 
 $(function() {
   Engine.init();
-  Engine.turnLightsOff();
 });
--- script.js
+++ script.js
@@ -3,32 +3,4 @@ function openPage(url) {
     const iframe = document.getElementById('iframe');
     iframe.src = url;
     iframeContainer.style.display = 'block';
-    
-    if (iframe.requestFullscreen) {
-      iframe.requestFullscreen();
-    } else if (iframe.webkitRequestFullscreen) { 
-      iframe.webkitRequestFullscreen(); // Safari
-    } else if (iframe.msRequestFullscreen) { 
-      iframe.msRequestFullscreen(); // IE11
-    }
-  }
-  
-  // Close fullscreen if ESC key is pressed
-  document.addEventListener('keydown', (event) => {
-    if (event.key === 'Escape') {
-      closeFullscreen();
-    }
-  });
-  
-  function closeFullscreen() {
-    if (document.exitFullscreen) {
-      document.exitFullscreen();
-    } else if (document.webkitExitFullscreen) {
-      document.webkitExitFullscreen(); // Safari
-    } else if (document.msExitFullscreen) {
-      document.msExitFullscreen(); // IE11
-    }
-    
-    const iframeContainer = document.getElementById('iframe-container');
-    iframeContainer.style.display = 'none';
   }
