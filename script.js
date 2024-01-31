220a76447ef5c4fdf62fc72cc1fbf4683bacd788
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-01-31 14:08:30 -0500

    beutify js,

--- script.js
+++ script.js
@@ -4,24 +4,25 @@ iframeContainer.style.display = 'block';
 
 if (iframe.requestFullscreen) {
     iframe.requestFullscreen();
-  } else if (iframe.webkitRequestFullscreen) { 
+} else if (iframe.webkitRequestFullscreen) {
     iframe.webkitRequestFullscreen(); // Safari
-  } else if (iframe.msRequestFullscreen) { 
+} else if (iframe.msRequestFullscreen) {
     iframe.msRequestFullscreen(); // IE11
 }
 document.addEventListener('keydown', (event) => {
-  if (event.key === 'Escape') {
-    closeFullscreen();
-}
+    if (event.key === 'Escape') {
+        closeFullscreen();
+    }
 });
+
 function closeFullscreen() {
-  if (document.exitFullscreen) {
-    document.exitFullscreen();
-  } else if (document.webkitExitFullscreen) {
-    document.webkitExitFullscreen(); // Safari
-  } else if (document.msExitFullscreen) {
-    document.msExitFullscreen(); // IE11
-}
-const iframeContainer = document.getElementById('iframe-container');
-  iframeContainer.style.display = 'none';
+    if (document.exitFullscreen) {
+        document.exitFullscreen();
+    } else if (document.webkitExitFullscreen) {
+        document.webkitExitFullscreen(); // Safari
+    } else if (document.msExitFullscreen) {
+        document.msExitFullscreen(); // IE11
+    }
+    const iframeContainer = document.getElementById('iframe-container');
+    iframeContainer.style.display = 'none';
 }
