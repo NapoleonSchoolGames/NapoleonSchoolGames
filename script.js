0d2e7afc06b001fdbe5eb731c1081668fdcb2744
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-01-31 08:01:04 -0500

    try and make games page work via iframes

--- script.js
+++ script.js
@@ -1,7 +1,6 @@
 function openPage(url) {
     const iframeContainer = document.getElementById('iframe-container');
     const iframe = document.getElementById('iframe');
-    
     iframe.src = url;
     iframeContainer.style.display = 'block';
     
