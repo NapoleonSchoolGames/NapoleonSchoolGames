ec2cbf2f70c7ba6c0ee1fbaf235d877cdde31c87
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-03-18 08:11:14 -0400

    just work pls

--- games/halflife/mods.js
+++ games/halflife/mods.js
@@ -1,7 +1,7 @@
-// filename, name, bytes, parts, part size
+// filename, name, bytes, parts + 1, part size
 zipMods = [
 	['halva_en.zip', 'Half-Life EN (MENU LAGS) (214M)', 214283501, 21, 10485760],
-	['uplinked.zip', 'Uplinked (15M)', 16147046, 1, 16147046]
+	['uplinked.zip', 'Uplinked (15M)', 16147046, 2, 16147046]
 ];
 
 pkgMods = [
@@ -12,11 +12,11 @@ selectZip.addEventListener('change', function(){
 	if(selectZip.value=="halva_en.zip")
 	{
 		//alert('Bugs: menu lagging.');
-		iArgs.value="-dev 1 -game valve";
+		iArgs.value="-dev 0 -game valve";
 	}
 	if(selectZip.value=="uplinked.zip")
 	{
 		//alert('Bugs: menu lagging.');
-		iArgs.value="-dev 1 -game uplinked";
+		iArgs.value="-dev 0 -game uplinked";
 	}
 });
