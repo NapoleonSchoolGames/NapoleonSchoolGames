7b3b9af6a7e92e75e0b6caf9cf0a66062e8a77d9
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-03-18 07:22:28 -0400

    maybe

--- games/halflife/mods.js
+++ games/halflife/mods.js
@@ -1,8 +1,5 @@
 zipMods = [
-	['halva_en.zip', 'Half-Life EN (MENU LAGS) (214M)', 214283501, 21, 10485760],
-	['opposing_force_en.zip', 'Opposing Force EN (BUGGED (old ver) + MENU LAGS) (221M)', 221191140, 22, 10485760],
-	['halva_rus.zip', 'Half-Life RUS (304M)', 303881453, 29, 10485760, 10485760],
-	['opposing_force_rus.zip', 'Opposing Force RUS (BUGGED (old ver)) (224M)', 223479923, 22, 10485760]
+	['halva_en.zip', 'Half-Life EN (MENU LAGS) (214M)', 214283501, 21, 10485760]
 ];
 
 pkgMods = [
