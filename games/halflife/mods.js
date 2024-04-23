60b0d89f87ecfbcbbdace2fb421590142ecee890
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-04-23 11:26:24 -0400

    minify

--- games/halflife/mods.js
+++ games/halflife/mods.js
@@ -1,17 +1,1 @@
-// filename, name, bytes, parts, part size
-zipMods = [
-	['halva_en.zip', 'Half-Life EN (MENU LAGS) (214M)', 214283501, 21, 10485760],
-	['halva_en.zip', 'Half-Life EN (MENU LAGS) (214M) (DUPLICATE FOR FIX)', 214283501, 21, 10485760]
-];
-
-pkgMods = [
-];
-var selectZip=document.getElementById('selectZip');
-var iArgs=document.getElementById('iArgs');
-selectZip.addEventListener('change', function(){
-	if(selectZip.value=="halva_en.zip")
-	{
-		//alert('Bugs: menu lagging.');
-		iArgs.value="-dev 0 -game valve";
-	}
-});
+zipMods=[['halva_en.zip','Half-Life EN (MENU LAGS) (214M)',214283501,21,10485760],['halva_en.zip','Half-Life EN (MENU LAGS) (214M) (DUPLICATE FOR FIX)',214283501,21,10485760]];pkgMods=[];var selectZip=document.getElementById('selectZip');var iArgs=document.getElementById('iArgs');selectZip.addEventListener('change',function(){if(selectZip.value=="halva_en.zip"){iArgs.value="-dev 0 -game valve"}})
