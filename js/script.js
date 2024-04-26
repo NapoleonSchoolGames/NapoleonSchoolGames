b55517c3cd1306c26d4e75dc53bf0239ecb75795
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-04-26 13:55:14 -0400

    bugfix, move code

--- js/script.js
+++ js/script.js
@@ -3,21 +3,6 @@ function openPage(url) {
   const iframe = document.getElementById('iframe');
   const buttons = document.getElementById('game-selection')
   //Hide buttons on call
-  buttons.style.display = 'none';
-  const currentSrc = new URL(iframe.src, window.location).href;
-  const targetSrc = new URL(url, window.location).href;
-  //Set iframe
-  if (currentSrc !== targetSrc) {
-    iframe.src = url;
-  }
-  //fullscreen
-  if (iframe.requestFullscreen) {
-    iframe.requestFullscreen();
-  } else if (iframe.webkitRequestFullscreen) { 
-    iframe.webkitRequestFullscreen(); 
-  } else if (iframe.msRequestFullscreen) { 
-    iframe.msRequestFullscreen();
-  }
   Swal.fire({
     title: "Please consider donating to help me develop the site",
     text: "If you don't want to, thats cool! Just wait 5 seconds for this to go away.",
@@ -34,6 +19,21 @@ function openPage(url) {
       window.location.href = `https://www.youtube.com/watch?v=dQw4w9WgXcQ`
     }
   }); 
+  buttons.style.display = 'none';
+  const currentSrc = new URL(iframe.src, window.location).href;
+  const targetSrc = new URL(url, window.location).href;
+  //Set iframe
+  if (currentSrc !== targetSrc) {
+    iframe.src = url;
+  }
+  //fullscreen
+  if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.webkitRequestFullscreen) { 
+    iframe.webkitRequestFullscreen(); 
+  } else if (iframe.msRequestFullscreen) { 
+    iframe.msRequestFullscreen();
+  }
 }
 function openSWF(swf) {
   openPage(`./flash.html?swf=${swf}`);
