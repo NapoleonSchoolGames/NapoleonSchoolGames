5f52813f3518cc64aa46163b0ca9f790154e7341
Dragonterror <144467487+DragonterrorOfficial@users.noreply.github.com>
2024-04-29 09:35:14 -0400

    popup on game (please)

--- js/script.js
+++ js/script.js
@@ -1,78 +1,4 @@
-function openPage(url) {
-  const iframeContainer = document.getElementById('iframe-container');
-  const iframe = document.getElementById('iframe');
-  const buttons = document.getElementById('game-selection')
-  //Hide buttons on call
-  buttons.style.display = 'none';
-  const currentSrc = new URL(iframe.src, window.location).href;
-  const targetSrc = new URL(url, window.location).href;
-  //Set iframe
-  if (currentSrc !== targetSrc) {
-    iframe.src = url;
-  }
-  //fullscreen
-  if (iframe.requestFullscreen) {
-    iframe.requestFullscreen();
-  } else if (iframe.webkitRequestFullscreen) { 
-    iframe.webkitRequestFullscreen(); 
-  } else if (iframe.msRequestFullscreen) { 
-    iframe.msRequestFullscreen();
-  }
-  loadPopup()
-}
-function openSWF(swf) {
-  openPage(`./flash.html?swf=${swf}`);
-}
-function fullscreenchanged(event) {
-  const iframeContainer = document.getElementById('iframe-container');
-  const iframe = document.getElementById('iframe');
-  const buttons = document.getElementById('game-selection') 
-  //if not fullscreen, show buttons
-  if (document.fullscreenElement) {
-  } else {
-    buttons.style.display = 'block';
-  }
-}
-function searchGames() {
-  var input, filter, gameButtons, i;
-  input = document.getElementById('search-bar');
-  filter = input.value.toLowerCase().replace(/\s+/g, '');
-  gameButtons = document.getElementsByClassName("game-button");
-  //check if the text matches any game's image name (triggers on keyUp)
-  for (i = 0; i < gameButtons.length; i++) {
-      if (gameButtons[i].src.indexOf(filter) > -1) {
-          gameButtons[i].style.display = '';
-      } else {
-          gameButtons[i].style.display = 'none';
-      }
-  }
-}
-async function generateNews() {
-  const response = await fetch('./json/news.json');
-  const newsData = await response.json();
-  const newsContainer = document.getElementById('news-container');
-  //for all news "Blocks" call createNewsElement with the title/content, then appends it.
-  newsData.forEach(news => {
-    const newsElement = createNewsElement(news.title, news.content);
-    newsContainer.appendChild(newsElement);
-  });
-}
-function createNewsElement(title, content) {
-  const element = document.createElement('div');
-  element.classList.add('news');
-  //create element
-  const titleElement = document.createElement('h2');
-  //add content
-  titleElement.textContent = title;
-  //add class
-  titleElement.classList.add('news-title');
-  const contentElement = document.createElement('span');
-  contentElement.textContent = content;
-  contentElement.classList.add('news-content');
-  element.appendChild(titleElement);
-  element.appendChild(contentElement);
-  return element;
-}
+//init functions
 function loadGames() {
   const gameSelectionDiv = document.getElementById('game-selection');
   fetch('./json/games.json')
@@ -105,9 +31,46 @@ function loadExploits() {
     })
     .catch(error => alert('Error fetching exploits:', error));
 }
-function loadPopup() {
-  //amount of popups + 1
-  var popup = Math.floor(Math.random() * 2);
+
+//news functions
+async function generateNews() {
+  const response = await fetch('./json/news.json');
+  const newsData = await response.json();
+  const newsContainer = document.getElementById('news-container');
+  //for all news "Blocks" call createNewsElement with the title/content, then appends it.
+  newsData.forEach(news => {
+    const newsElement = createNewsElement(news.title, news.content);
+    newsContainer.appendChild(newsElement);
+  });
+}
+function createNewsElement(title, content) {
+  const element = document.createElement('div');
+  element.classList.add('news');
+  //create element
+  const titleElement = document.createElement('h2');
+  //add content
+  titleElement.textContent = title;
+  //add class
+  titleElement.classList.add('news-title');
+  const contentElement = document.createElement('span');
+  contentElement.textContent = content;
+  contentElement.classList.add('news-content');
+  element.appendChild(titleElement);
+  element.appendChild(contentElement);
+  return element;
+}
+
+//game  opening functions
+//loadGame/loadFlash calls loadPopup, which calls openPage
+function loadGame(url) {
+  loadPopup(url)
+}
+function loadFlash(swf) {
+  loadGame(`./flash.html?swf=${swf}`)
+}
+function loadPopup(url) {
+  var popups = 2
+  var popup = Math.floor(Math.random() * (popups - 1));
   if (popup == 0) {
     var title = "popup1"
     var text = "popup1"
@@ -121,6 +84,57 @@ function loadPopup() {
   Swal.fire({
     title: title,
     text: text,
-    icon: icon
+    icon: icon,
+    showConfirmButton: false,
+    timer: 3000,
+    timerProgressBar: true,
+    didDestroy: openPage(url)
   });
+}
+function openPage(url) {
+  const iframeContainer = document.getElementById('iframe-container');
+  const iframe = document.getElementById('iframe');
+  const buttons = document.getElementById('game-selection')
+  //Hide buttons on call
+  buttons.style.display = 'none';
+  const currentSrc = new URL(iframe.src, window.location).href;
+  const targetSrc = new URL(url, window.location).href;
+  //Set iframe
+  if (currentSrc !== targetSrc) {
+    iframe.src = url;
+  }
+  //fullscreen
+  if (iframe.requestFullscreen) {
+    iframe.requestFullscreen();
+  } else if (iframe.webkitRequestFullscreen) { 
+    iframe.webkitRequestFullscreen(); 
+  } else if (iframe.msRequestFullscreen) { 
+    iframe.msRequestFullscreen();
+  }
+}
+
+//user functions
+function searchGames() {
+  var input, filter, gameButtons, i;
+  input = document.getElementById('search-bar');
+  filter = input.value.toLowerCase().replace(/\s+/g, '');
+  gameButtons = document.getElementsByClassName("game-button");
+  //check if the text matches any game's image name (triggers on keyUp)
+  for (i = 0; i < gameButtons.length; i++) {
+      if (gameButtons[i].src.indexOf(filter) > -1) {
+          gameButtons[i].style.display = '';
+      } else {
+          gameButtons[i].style.display = 'none';
+      }
+  }
+}
+function fullscreenchanged(event) {
+  const iframeContainer = document.getElementById('iframe-container');
+  const iframe = document.getElementById('iframe');
+  const buttons = document.getElementById('game-selection') 
+  //if not fullscreen, show buttons
+  if (document.fullscreenElement) {
+  } else {
+    buttons.style.display = 'block';
+  }
 }
